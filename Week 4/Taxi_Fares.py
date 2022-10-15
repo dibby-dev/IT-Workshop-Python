57ba57ec3c11167768e35451e386c9e805a910d1
Dibbyarup Dutta <work.dutta22@gmail.com>
2022-10-15 11:11:03 +0530

    updated

--- Week 4/Taxi_Fares.py
+++ Week 4/Taxi_Fares.py
@@ -1,7 +1,8 @@
 # Question - 1
 
-distance = float(input("Enter the distance travelled (in m): "))
-distance = distance/140
-BASE_FARE = 4.00
-total_fare = BASE_FARE + (distance*0.25)
-print(f"Total Fare = ${total_fare}")
+if __name__ == "__main__":
+    distance = float(input("Enter the distance travelled (in m): "))
+    distance = distance/140
+    BASE_FARE = 4.00
+    total_fare = BASE_FARE + (distance*0.25)
+    print(f"Total Fare = ${total_fare}")
